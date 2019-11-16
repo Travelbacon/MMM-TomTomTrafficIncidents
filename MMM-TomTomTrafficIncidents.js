46610f1664d85782d94a5aafb5de981c4f528739
Joris Spekreijse <algemeen@jorisspekreijse.nl>
2019-11-16 12:22:03 +0100

    Complete

--- MMM-TomTomTrafficIncidents.js
+++ MMM-TomTomTrafficIncidents.js
@@ -1,14 +1,31 @@
 Module.register("MMM-TomTomTrafficIncidents",{
 
 	defaults: {
+		requiresVersion: "2.8.0",  //Developped and tested with this version.
 		key: "",
 		height: "75vh",
 		width: "75vh",
 		traffic: "relative",
-		refresh: 30000, //default setting from TomTom. <30.000 will be treated as 30.000.
+		refresh: (15 * 60 * 1000), //Human readable for every 15 minutes.
 		showIncidents: true,
 		showTraffic: true,
-		TTVersion: "5.39.0" //internal solution to quickly change version of TomTom API to a new version.
+		remoteTTCSSJS: false,
+		TTVersion: "5.39.0" //Internal solution to quickly change version of TomTom API to a new version.
+	},
+		
+	getStyles: function() {
+		//Not getting data from Tomtom, will stall the module. Give the user the choice for local.
+		if(this.config.remoteTTCSSJS) {
+			return [
+				"https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/" +  this.config.TTVersion + "/maps/maps.css",
+				"https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/" +  this.config.TTVersion + "/maps/css-styles/traffic-incidents.css"
+			]
+		} else {
+			return [
+				this.file("tomtom-international-web-sdk-maps/maps.css"),
+				this.file("tomtom-international-web-sdk-maps/traffic-incidents.css")
+			]
+		}
 	},
 	
 	start: function() {
@@ -27,23 +44,13 @@ Module.register("MMM-TomTomTrafficIncidents",{
 		wrapper.style.height = this.config.height;
         wrapper.style.width = this.config.width;
 		
-		alert("Version :" + this.config.TTVersion);
-	
-		let mapstyle = document.createElement("link");
-		mapstyle.type = "text/css";
-		mapstyle.rel = "stylesheet";
-		mapstyle.href = "https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/" + this.config.TTVersion + "/maps/maps.css";
-		document.head.appendChild(mapstyle);
-		
-		let trafficstyle = document.createElement("link");
-		trafficstyle.type = "text/css";
-		trafficstyle.rel = "stylesheet";
-		trafficstyle.href = "https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/" + this.config.TTVersion + "/maps/css-styles/traffic-incidents.css";
-		document.head.appendChild(trafficstyle);
-		
-		let script = document.createElement("script");
+		let script = document.createElement("script"); //Getscripts is not working in this module.
 		script.type = "text/javascript";
-		script.src = "https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/" + this.config.TTVersion + "/maps/maps-web.min.js";
+		if(this.config.remoteTTCSSJS) {
+			script.src = "https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/" + this.config.TTVersion + "/maps/maps-web.min.js";
+		} else {
+			script.src = this.file("tomtom-international-web-sdk-maps/maps-web.min.js");
+		}
 		script.setAttribute('defer','');
 		script.setAttribute('async','');
 		document.body.appendChild(script);
@@ -51,7 +58,7 @@ Module.register("MMM-TomTomTrafficIncidents",{
 		var self = this;	//Fixme: why?			
 				
     	script.onload = function () {
-			tt.setProductInfo('MagicMirror TomTomTrafficIncidents', '0.9');
+			tt.setProductInfo('MagicMirror TomTom Traffic & Incidents', '1.0');
 			let map = new tt.map({
 				key: self.config.key,
 				container: 'map',
@@ -66,14 +73,14 @@ Module.register("MMM-TomTomTrafficIncidents",{
 				if (self.config.showTraffic) {
 					map.addTier(new tt.TrafficFlowTilesTier({
 						key: self.config.key,
-						style: 'tomtom://raster/1/' + self.config.traffic,
+						style: 'tomtom://vector/1/' + self.config.traffic,
 						refresh: self.config.refresh
 					}));
 				}
 				if (self.config.showIncidents) {
 					map.addTier(new tt.TrafficIncidentTier({
 						key: self.config.key,
-						incidentTiles: { style: 'tomtom://raster/1/s1'},
+						incidentTiles: { style: 'tomtom://vector/1/s1'},
 						incidentDetails: {style: 'night'},  //night doesn't group the icons.
 						refresh: self.config.refresh
 					}))
